86850cf6c126c1273cb4726414516cca3a8afe02
cromachina <82557197+cromachina@users.noreply.github.com>
2023-09-15 16:45:54 -0400

    License in script

--- pawoo-flex-columns.js
+++ pawoo-flex-columns.js
@@ -7,6 +7,7 @@
 // @match        https://pawoo.net/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=pawoo.net
 // @grant        GM_addStyle
+// @license      MIT
 // ==/UserScript==
 
 (function() {
