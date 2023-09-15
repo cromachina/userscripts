6baf161b417e55897676953325b398b5a4cb9ca0
cromachina <82557197+cromachina@users.noreply.github.com>
2023-09-15 16:44:42 -0400

    Add pawoo flex columns

---
+++ pawoo-flex-columns.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Pawoo flex columns
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Make Pawoo columns expand to fit the page width.
+// @author       Cro
+// @match        https://pawoo.net/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=pawoo.net
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    GM_addStyle('.column { flex: 1 }');
+})();
