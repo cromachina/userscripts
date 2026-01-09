9eb40b5210532ab75b5f3a0b6bb29d2cf5b55e18
cromachina <82557197+cromachina@users.noreply.github.com>
2026-01-09 11:44:20 -0500

    Fix and simplify pixiv dash stats script

--- pixiv-extended-dashboard-statistics.js
+++ pixiv-extended-dashboard-statistics.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Pixiv extended dashboard statistics.
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @description  Put view-ratio data on likes, bookmarks, etc.
 // @author       cro
 // @match        https://www.pixiv.net/*
@@ -24,7 +24,10 @@
         let get_container = function(node)
         {
             let maybe_child = node.querySelector(`span[id=${id_name}]`);
-            if (maybe_child) return maybe_child;
+            if (maybe_child)
+            {
+                return maybe_child;
+            }
             let child = document.createElement('span');
             child.id = id_name;
             let container = node.querySelector(container_query) || node;
@@ -40,66 +43,30 @@
 
         return function(target_node, denom_node)
         {
-            if (target_node && denom_node) get_container(target_node).textContent = ` (${(count(target_node) / count(denom_node) * 100).toFixed(2)}%)`;
+            if (target_node && denom_node)
+            {
+                let value = (count(target_node) / count(denom_node) * 100).toFixed(2);
+                get_container(target_node).textContent = ` (${value}%)`;
+            }
         };
     };
 
-    let query_array = (selector) => Array.from(document.querySelectorAll(selector).values());
-    let desktop_class = 'f4332f9e';
-    let mobile_class = 'cb68f05f';
+    let set_ratio = make_set_ratio(`span:last-of-type:not(#${id_name})`);
+    let paths = ['/dashboard', '/dashboard/works'];
 
     let process_all = function()
     {
-        if (window.location.pathname == "/dashboard/works")
+        if (paths.includes(window.location.pathname))
         {
-            // Desktop view
-            if (document.querySelector(`[class*=sc-${desktop_class}-0]`))
+            let illusts = Array.from(document.querySelectorAll('a[href*="/artworks/')).map(x=>x.href.match(/\d+/)[0]);
+
+            for (let id of illusts)
             {
-                let set_ratio = make_set_ratio('a');
-                let indices = query_array('div[class*=gtm-dashboard-works-sort-select]');
-                let get_index = function(name)
-                {
-                    let node = document.querySelector(`div[class*=gtm-dashboard-works-sort-select-${name}]`);
-                    let i = indices.indexOf(node);
-                    return i == -1 ? null : i - 1;
-                };
-                let view_index = get_index('view');
-                let like_index = get_index('rating');
-                let bookmark_index = get_index('bookmark');
-                let rows = _.chunk(query_array(`div[class*=sc-${desktop_class}-25]`), indices.length + 1);
-                rows = rows.map(row => row.slice(2));
-                for (let row of rows)
-                {
-                    let view = row[view_index];
-                    set_ratio(row[like_index], view);
-                    set_ratio(row[bookmark_index], view);
-                }
-            }
-            // Mobile view
-            else if (document.querySelector(`[class*=sc-${mobile_class}-3]`))
-            {
-                let set_ratio = make_set_ratio(null);
-                let cell_nodes = query_array(`div[class*=sc-${mobile_class}-6]`);
-                for (let cell of cell_nodes)
-                {
-                    let view = cell.querySelector('a[href*=access]');
-                    let like = cell.querySelector('a[href*=rating]');
-                    let bookmark = cell.querySelector('a[href*=bookmark]');
-                    set_ratio(like, view);
-                    set_ratio(bookmark, view);
-                }
-            }
-        }
-        else if (window.location.pathname == "/dashboard")
-        {
-            let set_ratio = make_set_ratio(`span:last-of-type:not(#${id_name})`);
-            let views = query_array('a.gtm-dashboard-home-latest-works-number-link-view');
-            let likes = query_array('a.gtm-dashboard-home-latest-works-number-link-like');
-            let bookmarks = query_array('a.gtm-dashboard-home-latest-works-number-link-bookmark');
-            for (let [view, like, bookmark] of _.zip(views, likes, bookmarks))
-            {
-                set_ratio(like, view);
-                set_ratio(bookmark, view);
+                let likes = document.querySelector(`a[href="/dashboard/report/artworks?section=rating&id=${id}`);
+                let views = document.querySelector(`a[href="/dashboard/report/artworks?section=access&id=${id}`);
+                let bookmarks = document.querySelector(`a[href="/bookmark_detail.php?illust_id=${id}`);
+                set_ratio(likes, views);
+                set_ratio(bookmarks, views);
             }
         }
     };
